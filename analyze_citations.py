565e457574272d774f40e8371e3526fd515d7729
klieret <klieret@users.noreply.github.com>
2017-05-30 22:43:47 +0200

    clustering

--- analyze_citations.py
+++ analyze_citations.py
@@ -3,6 +3,8 @@
 import os
 import re
 import collections
+import sys
+import datetime
 
 
 material_folder = "/home/kilian/Documents/db_master/material/material/"
@@ -10,14 +12,35 @@ citation_folder = "/home/kilian/Dropbox/db_master/material/marc/"
 
 clusters = collections.defaultdict(set)
 
-for root, dirs, files in os.walk(material_folder):
-    for file in files:
-        if os.path.splitext(file)[1] in [".pdf", ".ps", ".djvu"]:
-            mid = os.path.basename(file)[0:4]
-            clustername = "ROOT"
-            if os.path.basename(root):
-                clustername = os.path.basename(root)
-            clusters[clustername].add(mid)
+do_clusters = False
+hide_unrelated = True
+max_cluster_depth = 2
+
+if do_clusters:
+    for root, dirs, files in os.walk(material_folder):
+        for file in files:
+            if os.path.splitext(file)[1] in [".pdf", ".ps", ".djvu"]:
+                mid = os.path.basename(file)[0:4]
+                clustername = os.path.join(*os.path.relpath(root, material_folder).split(os.sep)[:max_cluster_depth])
+                if not clustername:
+                    clustername = "ROOT"
+                clusters[clustername].add(mid)
+                # NOTE: Not supported to have node in several clusters
+                # for i in range(len(relpath)):
+                #     partial_path = relpath.split(os.sep)[:i]
+                #     if not partial_path:
+                #         partial_path = ["ROOT"]
+                #     # print(partial_path)
+                #     partial_path = os.path.join(*partial_path)
+                #     clustername = os.path.basename(partial_path)
+                #     # if not clustername:
+                #     #     clustername = "ROOT"
+                #     print(clustername)
+                #     clusters[clustername].add(mid)
+
+
+print(clusters)
+#sys.exit(1)
 
 record_regex = re.compile("/record/([0-9]*)")
 
@@ -40,12 +63,15 @@ for filename in os.listdir(citation_folder):
 
 my_dots = set()
 for dot_connection in dot_connections:
-    my_dots.add(dot_connection[0])
+    my_dots.add(hep_to_mref[dot_connection[0]])
 
 my_dot_connections = []
 for dot_connection in dot_connections:
-    if dot_connection[1] in my_dots:
-        my_dot_connections.append(dot_connection)
+    if not dot_connection[1] in hep_to_mref:
+        continue
+    if hep_to_mref[dot_connection[1]] in my_dots:
+        my_dot_connections.append((hep_to_mref[dot_connection[0]],
+                                   hep_to_mref[dot_connection[1]]))
 
 # dot_txt = """digraph g {
 #  ratio="fill";
@@ -53,19 +79,33 @@ for dot_connection in dot_connections:
 #  margin=0;\n"""
 dot_txt = ""
 
-dot_txt += """digraph g {\n"""
+dot_txt += """digraph g {
+              graph [label="Sources Network as of %s %s", fontsize=100];
+              node[fontsize=50, fontcolor="red", fontname=Courier, shape=box];
+              ratio="fill";
+              //size="11.692,8.267"; 
+              size="16.53,11.69";
+              \n""" % (str(datetime.date.today()), str(datetime.datetime.now().time()))
 
 for dot_connection in my_dot_connections:
-    dot_txt += '\t"{}" -> "{}"; \n'.format(hep_to_mref[dot_connection[0]],
-                                       hep_to_mref[dot_connection[1]])
+    dot_txt += '\t"{}" -> "{}"; \n'.format(dot_connection[0], dot_connection[1])
 
 for cluster, items in clusters.items():
     dot_txt += '\tsubgraph "cluster_{}" {{\n'.format(cluster)
+    dot_txt += """\tfontname=Courier;
+    \tfontcolor=red;
+    \tpenwidth=3;
+    \tfontsize=50;
+    \tcolor="red";
+    \tstyle="filled";
+    \tfillcolor="gray97";
+    """
     dot_txt += '\tlabel="{}";\n'.format(cluster)
+    #dot_txt += '\tstyle="filled";'
     for item in items:
-        dot_txt += '\t\t"{}";\n'.format(item)
+        if item in my_dots or not hide_unrelated:
+            dot_txt += '\t\t"{}";\n'.format(item)
     dot_txt += "\t}\n"
-
 
 dot_txt += "}"
 
