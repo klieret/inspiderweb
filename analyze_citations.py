418a573c115bdf735d23c25d4c9743cf08c8f877
klieret <klieret@users.noreply.github.com>
2017-06-02 19:04:59 +0200

    quick save; startin to refactor; NOT FUNCTIONAL

--- analyze_citations.py
+++ analyze_citations.py
@@ -18,31 +18,55 @@ do_clusters = True
 hide_unrelated = True
 max_cluster_depth = 2
 
-if do_clusters:
-    for root, dirs, files in os.walk(material_folder):
-        for file in files:
-            if os.path.splitext(file)[1] in [".pdf", ".ps", ".djvu"]:
-                mid = os.path.basename(file)[0:4]
-                clustername = os.path.join(*os.path.relpath(root, material_folder).split(os.sep)[:max_cluster_depth])
-                if not clustername:
-                    clustername = "ROOT"
-                clusters[clustername].add(mid)
-                # NOTE: Not supported to have node in several clusters
-                # for i in range(len(relpath)):
-                #     partial_path = relpath.split(os.sep)[:i]
-                #     if not partial_path:
-                #         partial_path = ["ROOT"]
-                #     # print(partial_path)
-                #     partial_path = os.path.join(*partial_path)
-                #     clustername = os.path.basename(partial_path)
-                #     # if not clustername:
-                #     #     clustername = "ROOT"
-                #     print(clustername)
-                #     clusters[clustername].add(mid)
+seeds = []  # initial papers I'm interested in
+
+
+# if do_clusters:
+#     for root, dirs, files in os.walk(material_folder):
+#         for file in files:
+#             if os.path.splitext(file)[1] in [".pdf", ".ps", ".djvu"]:
+#                 mid = os.path.basename(file)[0:4]
+#                 clustername = os.path.join(*os.path.relpath(root, material_folder).split(os.sep)[:max_cluster_depth])
+#                 if not clustername:
+#                     clustername = "ROOT"
+#                 clusters[clustername].add(mid)
+#                 # NOTE: Not supported to have node in several clusters
+#                 # for i in range(len(relpath)):
+#                 #     partial_path = relpath.split(os.sep)[:i]
+#                 #     if not partial_path:
+#                 #         partial_path = ["ROOT"]
+#                 #     # print(partial_path)
+#                 #     partial_path = os.path.join(*partial_path)
+#                 #     clustername = os.path.basename(partial_path)
+#                 #     # if not clustername:
+#                 #     #     clustername = "ROOT"
+#                 #     print(clustername)
+#                 #     clusters[clustername].add(mid)
 
 
 #print(clusters)
 #sys.exit(1)
+
+
+class Record(object):
+    def __init__(self, inspire_url, label=None):
+        self.inspire_url = inspire_url
+        self.label = label
+        # if label:
+        #     self.label = label
+        # else:
+        #     self.label = self.inspire_url.split('/')[-1]
+
+
+def parse_records(string):
+    # goes fro string, looking for every record url
+    pass
+
+def get_cited_by(record):
+    pass
+
+def get_references(record):
+    pass
 
 record_regex = re.compile("/record/([0-9]*)")
 
@@ -75,50 +99,75 @@ for dot_connection in dot_connections:
         my_dot_connections.append((hep_to_mref[dot_connection[0]],
                                    hep_to_mref[dot_connection[1]]))
 
-# dot_txt = """digraph g {
-#  ratio="fill";
-#  size="8.3,11.7!";
-#  margin=0;\n"""
-dot_txt = ""
 
-dot_txt += """digraph g {
-              graph [label="Sources Network as of %s %s", fontsize=100];
-              node[fontsize=10, fontcolor="black", fontname=Arial, shape=box];
-              //ratio="1:1";
-              //node[fontsize=50, fontcolor="red", fontname=Courier, shape=box];
-              ratio="fill";
-              //size="11.692,8.267"; 
-              //size="16.53,11.69"; //a3
-              //size="33.06,11.69"
-              \n""" % (str(datetime.date.today()), str(datetime.datetime.now().time()))
+class DotGraph(object):
+    def __init__(self):
+        self.dot_str = ""
+
+    def start_digraph(self):
+        self.dot_str += \
+            ("digraph g {\n"
+             "\tgraph [label=\"Network as of {date} {time}\", fontsize=100];\n"
+             "\tnode[fontsize=10, fontcolor=black, fontname=Arial, shape=box];\n"
+             "//\tratio=\"1:1\";\n"
+             "//\tnode[fontsize=50, fontcolor=\"red\", fontname=Courier, shape=box];\n"
+             "//\tratio=\"fill\";\n"
+             "//\tsize=\"11.692,8.267\"; \n"
+             "//\tsize=\"16.53,11.69\"; //a3\n"
+             "//\tsize=\"33.06,11.69\"\n"
+             "\n").format(date=str(datetime.date.today()),
+                          time=str(datetime.datetime.now().time()))
+
+    def end_digraph(self):
+        self.dot_str += "}"
+
+    def add_cluster(self):
+        raise NotImplemented
+
+    def add_connection(self, start, end):
+        self.dot_str += '\t"{}" -> "{}"; \n'.format(dot_connection[0],
+                                                    dot_connection[1])
+
+    def return_dot_str(self):
+        return self.dot_str
+
+    def write_to_file(self, filename):
+        with open(filename, "w") as dotfile:
+            dotfile.write(self.dot_str)
+
+dg = DotGraph()
+
+dg.start_digraph()
 
 for dot_connection in my_dot_connections:
-    dot_txt += '\t"{}" -> "{}"; \n'.format(dot_connection[0], dot_connection[1])
-
-for cluster, items in clusters.items():
-    dot_txt += '\tsubgraph "cluster_{}" {{\n'.format(cluster)
-    dot_txt += """\tfontname=Courier;
-    \tfontcolor=red;
-    \tpenwidth=3;
-    \tfontsize=50;
-    \tcolor="red";
-    \tstyle="filled";
-    \tfillcolor="gray97";
-    """
-    dot_txt += '\tlabel="{}";\n'.format(cluster)
-    #dot_txt += '\tstyle="filled";'
-    for item in items:
-        if item in my_dots or not hide_unrelated:
-            dot_txt += '\t\t"{}" [label="{}"];\n'.format(item, extract_field(mid_to_bib_paths[item], "title"))
-    dot_txt += "\t}\n"
-
-dot_txt += "}"
+    dg.add_connection(dot_connection[0], dot_connection[1])
 
 
 
-with open("dotfile.dot", "w") as dotfile:
-    dotfile.write(dot_txt)
+dg.end_digraph()
+dg.write_to_file("dotfile.dot")
 
-print("dot connections: {}".format(len(dot_connections)))
-print("My dots: {}".format(len(my_dots)))
-print("My dot connections: {}".format(len(my_dot_connections)))
+# for cluster, items in clusters.items():
+#     dot_txt += '\tsubgraph "cluster_{}" {{\n'.format(cluster)
+#     dot_txt += """\tfontname=Courier;
+#     \tfontcolor=red;
+#     \tpenwidth=3;
+#     \tfontsize=50;
+#     \tcolor="red";
+#     \tstyle="filled";
+#     \tfillcolor="gray97";
+#     """
+#     dot_txt += '\tlabel="{}";\n'.format(cluster)
+#     #dot_txt += '\tstyle="filled";'
+#     for item in items:
+#         if item in my_dots or not hide_unrelated:
+#             dot_txt += '\t\t"{}" [label="{}"];\n'.format(item, extract_field(mid_to_bib_paths[item], "title"))
+#     dot_txt += "\t}\n"
+# dot_txt += "}"
+
+
+
+
+# print("dot connections: {}".format(len(dot_connections)))
+# print("My dots: {}".format(len(my_dots)))
+# print("My dot connections: {}".format(len(my_dot_connections)))
