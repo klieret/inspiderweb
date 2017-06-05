9692252ad82c8c187f79dca06fff01484ec6291f
klieret <klieret@users.noreply.github.com>
2017-06-05 20:38:13 +0200

    remove test parts of record that have become obsolete

--- inspiderweb/test_record.py
+++ inspiderweb/test_record.py
@@ -17,23 +17,3 @@ class RecordTest(unittest.TestCase):
     def test_emptylabel(self):
         assert self.r.label == self.r.recid
 
-    def test_dl_remaining(self):
-        assert not self.r.references_dl
-        assert not self.r.citations_dl
-        assert not self.r.cocitations_dl
-
-    def test_dl_info(self):
-        self.r.get_info()
-        assert self.r.bibkey == "Maruyoshi:2016tqk"
-
-    def test_dl_references(self):
-        self.r.get_references()
-        assert self.r.references_dl
-        assert len(self.r.references) == 27
-
-    def test_dl_citations(self):
-        self.r.get_citations()
-        assert self.r.citations_dl
-        assert self.r.cocitations_dl
-        assert len(self.r.citations) == 10
-        assert len(self.r.cocitations) == 403
